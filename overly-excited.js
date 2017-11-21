94b60ff3e8c45b92b32f1e046ae1fd9a6d952d22
Sarah Morris <sarah.morris9@gmail.com>
2017-11-21 14:13:46 -0600

    Added JS file

---
+++ overly-excited.js
@@ -0,0 +1,26 @@
+// Create an array that contains the words in the sentence
+var sentence = [
+  {"The "},
+  {"walrus "},
+  {"danced "},
+  {"through "},
+  {"the "},
+  {"trees "},
+  {"in "},
+  {"the "},
+  {"light "},
+  {"of "},
+  {"the "}, 
+  {"moon."},
+
+];
+
+// The addExcitement function should accept the array as the sole argument
+function addExcitement (theWordArray) {
+    for (i = 0; i < sentence.length; i++) {
+
+    }
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence)
