8a9b6079e100040eb67dda86bff62e49e660e5b2
Sarah Morris <sarah.morris9@gmail.com>
2017-11-25 16:49:44 -0600

    Changed sentence and started for loop

--- overly-excited.js
+++ overly-excited.js
@@ -1,14 +1,18 @@
 // Create an array that contains the words in the sentence
 var sentence = [
-  "The ", "walrus ", "danced ", "through ", "the ", "trees ", "in ", "the ", "light ", "of ", "the ", "moon."
+  "The ", "puppy ", "howled ", "through ", "the ", "smoke ", "in ", "the ", "light ", "of ", "the ", "full ", "moon."
 ];
+var newSentence = "";
 
 // The addExcitement function should accept the array as the sole argument
 function addExcitement (theWordArray) {
-    for (i = 0; i < sentence.length; i++) {
-      console.log(sentence);
+    for (var i = 0; i < sentence.length; i++) {
+      newSentence += " ";
+      newSentence += sentence[i];
+
     }
+  console.log(newSentence);  
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence);
