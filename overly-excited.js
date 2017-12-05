73a7cf9f9e5895e92bca8ca82d56861676a253a0
Sarah Morris <sarah.morris9@gmail.com>
2017-12-05 15:52:58 -0600

    Completed challenge??

--- overly-excited.js
+++ overly-excited.js
@@ -1,18 +1,33 @@
 // Create an array that contains the words in the sentence
-var sentence = [
-  "The ", "puppy ", "howled ", "through ", "the ", "smoke ", "in ", "the ", "light ", "of ", "the ", "full ", "moon."
+let sentence = [
+  "The ", "puppy ", "howled ", "through ", "the ", "smoke ", "in ", "the ", "light ", "of ", "the ", "moon"
 ];
-var newSentence = "";
+let newSentence = "";
 
 // The addExcitement function should accept the array as the sole argument
-function addExcitement (theWordArray) {
-    for (var i = 0; i < sentence.length; i++) {
+function addExcitement (newSentence) {
+    for (let i = 0; i < sentence.length; i++) {
       newSentence += " ";
       newSentence += sentence[i];
-
-    }
-  console.log(newSentence);  
+      if ((i+1) % 3 === 0) {
+        newSentence += "!";
+      }
+      if ((i+1) % 6 === 0) {
+        newSentence += "!";
+      }
+      if ((i+1) % 9 === 0) {
+        newSentence += "!!";
+      }
+      if ((i+1) % 12 === 0) {
+        newSentence += "!!";
+      }
+      console.log(newSentence);
+    }  
 }
 
+
 // Invoke the function and pass in the array
-addExcitement(sentence);
+addExcitement(newSentence);
+
+
+
